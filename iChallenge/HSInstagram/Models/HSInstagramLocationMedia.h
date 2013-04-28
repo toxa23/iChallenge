37746223c62a7a1191ddd00041549c39b27341a9
Toxa <toxa@localhost.(none)>
2013-04-28 04:46:19 +0400

    Main directory

---
+++ iChallenge/HSInstagram/Models/HSInstagramLocationMedia.h
@@ -0,0 +1,20 @@
+//
+//  HSInstagramLocationMedia.h
+//  HSInstagramSample
+//
+//  Created by Harminder Sandhu on 12-01-20.
+//  Copyright (c) 2012 Pushbits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HSInstagram.h"
+
+@interface HSInstagramLocationMedia : NSObject
+
+@property (nonatomic, strong) NSString* thumbnailUrl;
+@property (nonatomic, strong) NSString* standardUrl;
+@property (nonatomic, assign) NSUInteger likes;
+
++ (void)getLocationMediaWithId:(NSString*)locationId block:(void (^)(NSArray *records))block;
+
+@end
